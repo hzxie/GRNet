0f5117493f1ec7b9eec50aeacc02ff6f74db7e94
Haozhe Xie <cshzxie@gmail.com>
2019-11-20 21:29:03 +0800

    Optimize the code to reduce memory usage.

--- extensions/gridding/setup.py
+++ extensions/gridding/setup.py
@@ -2,14 +2,14 @@
 # @Author: Haozhe Xie
 # @Date:   2019-11-13 10:51:33
 # @Last Modified by:   Haozhe Xie
-# @Last Modified time: 2019-11-18 16:17:40
+# @Last Modified time: 2019-11-20 21:06:03
 # @Email:  cshzxie@gmail.com
 
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(name='gridding',
-      version='1.0.0',
+      version='2.0.0',
       ext_modules=[
           CUDAExtension('gridding', [
               'gridding_cuda.cpp',
