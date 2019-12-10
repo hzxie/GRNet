9fe736e39dc374e7dd0c8d0566c564a10ffe6e99
Haozhe Xie <cshzxie@gmail.com>
2019-12-10 12:44:15 +0800

    Remove zeros in the predicted point cloud when calculating metrics.

--- extensions/chamfer_dist/__init__.py
+++ extensions/chamfer_dist/__init__.py
@@ -2,7 +2,7 @@
 # @Author: Thibault GROUEIX
 # @Date:   2019-08-07 20:54:24
 # @Last Modified by:   Haozhe Xie
-# @Last Modified time: 2019-12-10 10:07:32
+# @Last Modified time: 2019-12-10 12:42:41
 # @Email:  cshzxie@gmail.com
 
 import sys
@@ -23,7 +23,6 @@ class ChamferFunction(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_dist1, grad_dist2):
         xyz1, xyz2, idx1, idx2 = ctx.saved_tensors
-
         grad_xyz1, grad_xyz2 = chamfer.backward(xyz1, xyz2, idx1, idx2, grad_dist1, grad_dist2)
         return grad_xyz1, grad_xyz2
 
@@ -34,4 +33,11 @@ class ChamferDistance(torch.nn.Module):
         self.ignore_zeros = ignore_zeros
 
     def forward(self, xyz1, xyz2):
+        batch_size = xyz1.size(0)
+        if batch_size == 1 and self.ignore_zeros:
+            non_zeros1 = torch.sum(xyz1, dim=2).ne(0)
+            non_zeros2 = torch.sum(xyz2, dim=2).ne(0)
+            xyz1 = xyz1[non_zeros1].unsqueeze(dim=0)
+            xyz2 = xyz2[non_zeros2].unsqueeze(dim=0)
+
         return ChamferFunction.apply(xyz1, xyz2)
