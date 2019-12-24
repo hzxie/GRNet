38fe0733fd3d58d08068f45d479e207f151c4c30
Haozhe Xie <cshzxie@gmail.com>
2019-12-24 14:46:39 +0800

    Sample more features from RGNet for Refiner.

--- models/refiner.py
+++ models/refiner.py
@@ -2,7 +2,7 @@
 # @Author: Haozhe Xie
 # @Date:   2019-12-17 18:49:45
 # @Last Modified by:   Haozhe Xie
-# @Last Modified time: 2019-12-23 16:44:05
+# @Last Modified time: 2019-12-24 14:43:19
 # @Email:  cshzxie@gmail.com
 
 import torch
@@ -14,28 +14,28 @@ class Refiner(torch.nn.Module):
     def __init__(self, cfg):
         super(Refiner, self).__init__()
         self.fc1 = torch.nn.Sequential(
-            torch.nn.Linear(768, 768),
+            torch.nn.Linear(1792, 1792),
             torch.nn.ReLU()
         )
         self.fc2 = torch.nn.Sequential(
-            torch.nn.Linear(768, 384),
+            torch.nn.Linear(1792, 448),
             torch.nn.ReLU()
         )
         self.fc3 = torch.nn.Sequential(
-            torch.nn.Linear(384, 192),
+            torch.nn.Linear(448, 112),
             torch.nn.ReLU()
         )
-        self.fc4 = torch.nn.Linear(192, 24)
+        self.fc4 = torch.nn.Linear(112, 24)
 
     def forward(self, sparse_cloud, point_features):
         # print(sparse_cloud.size())      # torch.Size([batch_size, 2048, 3])
-        # print(point_features.size())    # torch.Size([batch_size, 2048, 768])
+        # print(point_features.size())    # torch.Size([batch_size, 2048, 1792])
         point_features = self.fc1(point_features)
-        # print(point_features.size())    # torch.Size([batch_size, 2048, 768])
+        # print(point_features.size())    # torch.Size([batch_size, 2048, 1792])
         point_features = self.fc2(point_features)
-        # print(point_features.size())    # torch.Size([batch_size, 2048, 384])
+        # print(point_features.size())    # torch.Size([batch_size, 2048, 448])
         point_features = self.fc3(point_features)
-        # print(point_features.size())    # torch.Size([batch_size, 2048, 192])
+        # print(point_features.size())    # torch.Size([batch_size, 2048, 112])
         point_offset = self.fc4(point_features).view(-1, 16384, 3)
         # print(point_features.size())    # torch.Size([batch_size, 16384, 3])
         sparse_cloud = sparse_cloud.unsqueeze(dim=2).repeat(1, 1, 8, 1).view(-1, 16384, 3)
