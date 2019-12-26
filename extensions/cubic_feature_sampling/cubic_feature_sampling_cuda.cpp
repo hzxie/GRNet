2fdb917dfaee714eb15436760fa355867d66c276
Haozhe Xie <cshzxie@gmail.com>
2019-12-26 13:55:49 +0800

    Add neighborhood_size to the cubic feature sampling layer.

--- extensions/cubic_feature_sampling/cubic_feature_sampling_cuda.cpp
+++ extensions/cubic_feature_sampling/cubic_feature_sampling_cuda.cpp
@@ -2,7 +2,7 @@
  * @Author: Haozhe Xie
  * @Date:   2019-12-19 17:04:38
  * @Last Modified by:   Haozhe Xie
- * @Last Modified time: 2019-12-19 22:05:34
+ * @Last Modified time: 2019-12-26 11:41:41
  * @Email:  cshzxie@gmail.com
  */
 
@@ -20,34 +20,42 @@
 
 std::vector<torch::Tensor> cubic_feature_sampling_cuda_forward(
   int scale,
+  int neighborhood_size,
   torch::Tensor ptcloud,
   torch::Tensor cubic_features,
   cudaStream_t stream);
 
 std::vector<torch::Tensor> cubic_feature_sampling_cuda_backward(
   int scale,
+  int neighborhood_size,
   torch::Tensor grad_point_features,
   torch::Tensor grid_pt_indexes,
   cudaStream_t stream);
 
 std::vector<torch::Tensor> cubic_feature_sampling_forward(
-  int scale, torch::Tensor ptcloud, torch::Tensor cubic_features) {
+  int scale,
+  int neighborhood_size,
+  torch::Tensor ptcloud,
+  torch::Tensor cubic_features) {
   CHECK_INPUT(ptcloud);
   CHECK_INPUT(cubic_features);
 
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-  return cubic_feature_sampling_cuda_forward(scale, ptcloud, cubic_features,
-                                             stream);
+  return cubic_feature_sampling_cuda_forward(scale, neighborhood_size, ptcloud,
+                                             cubic_features, stream);
 }
 
 std::vector<torch::Tensor> cubic_feature_sampling_backward(
-  int scale, torch::Tensor grad_point_features, torch::Tensor grid_pt_indexes) {
+  int scale,
+  int neighborhood_size,
+  torch::Tensor grad_point_features,
+  torch::Tensor grid_pt_indexes) {
   CHECK_INPUT(grad_point_features);
   CHECK_INPUT(grid_pt_indexes);
 
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-  return cubic_feature_sampling_cuda_backward(scale, grad_point_features,
-                                              grid_pt_indexes, stream);
+  return cubic_feature_sampling_cuda_backward(
+    scale, neighborhood_size, grad_point_features, grid_pt_indexes, stream);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- extensions/cubic_feature_sampling/setup.py
+++ extensions/cubic_feature_sampling/setup.py
@@ -2,14 +2,14 @@
 # @Author: Haozhe Xie
 # @Date:   2019-12-19 17:03:06
 # @Last Modified by:   Haozhe Xie
-# @Last Modified time: 2019-12-19 17:06:06
+# @Last Modified time: 2019-12-26 14:02:06
 # @Email:  cshzxie@gmail.com
 
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(name='cubic_feature_sampling',
-      version='1.0.0',
+      version='1.1.0',
       ext_modules=[
           CUDAExtension('cubic_feature_sampling', ['cubic_feature_sampling_cuda.cpp', 'cubic_feature_sampling.cu']),
       ],
